5c7d47fcd6ae5071c22f47c0e30c8eacc4c5e721
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-09 16:39:52 +0200

    feat: add generate 10 mail in vanilla js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
-axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-.then((response) => {
-    console.log(response.data)
-})
+for (let i = 0; i < 10; i++) {
+    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+    .then((response) => {
+        console.log(response.data)
+    })
+}
+
+
+
