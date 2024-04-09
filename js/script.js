867d91ee5e426b85f5caf680644e014b01d17a51
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-09 16:48:40 +0200

    feat: add base vue

--- js/script.js
+++ js/script.js
@@ -1,14 +1,25 @@
-for (let i = 0; i < 10; i++) {
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((response) => {
-        let valMail = response.data;
-        console.log(valMail);
+// for (let i = 0; i < 10; i++) {
+//     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+//     .then((response) => {
+//         let valMail = response.data;
+//         console.log(valMail);
 
-        let printResult = document.createElement('div');
-        console.log(printResult);
+//         let printResult = document.createElement('div');
+//         console.log(printResult);
 
-        })
-}
+//         })
+// }
+
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  }
+}).mount('#app')
 
 
 
