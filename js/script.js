58837a8156abf1ea869f1d41cbbb2a3c3c73cb4f
MatteoDiMartino <matteodimartino359@gmail.com>
2024-04-09 16:42:23 +0200

    feat: add value in a variable and create div

--- js/script.js
+++ js/script.js
@@ -1,8 +1,13 @@
 for (let i = 0; i < 10; i++) {
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
     .then((response) => {
-        console.log(response.data)
-    })
+        let valMail = response.data;
+        console.log(valMail);
+
+        let printResult = document.createElement('div');
+        console.log(printResult);
+
+        })
 }
 
 
